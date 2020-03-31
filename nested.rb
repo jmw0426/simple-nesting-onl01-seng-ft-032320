09eb0913b44694cac4b9f3e5b17dd4d6ed47d49a
Joshua Williams <jmw0426@hotmail.com>
2020-03-31 01:02:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
- dennis_new_info = "Assembly"
- programmer_hash[:dennis_ritchie][:known_for] = dennis_new_info
- dennis_new_info
+ 
+ programmer_hash[:dennis_ritchie][:languages] = "Unix", "Assembly"
+ programmer_hash
 end
